25372d920f46205db2ee75a8725cf418f214e450
yukun <yukun0507@outlook.jp>
2023-10-17 15:34:16 +0900

    update

--- fr_mandelbrot.c
+++ fr_mandelbrot.c
@@ -6,10 +6,41 @@
 /*   By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 22:29:40 by yushsato          #+#    #+#             */
-/*   Updated: 2023/10/15 22:30:21 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/10/17 15:34:11 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	fr_mandelbrot()
+#include "lib/minilibx_opengl/mlx.h"
+#include "lib/libft/libft.h"
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void	fr_mandelbrot(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
--- main.c
+++ main.c
@@ -6,27 +6,29 @@
 /*   By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 17:36:57 by yushsato          #+#    #+#             */
-/*   Updated: 2023/10/15 22:33:10 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/10/16 15:06:45 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// #define  DEBUG() ft_printf("[DEBUG] %d: %s\n", __LINE__, __func__)
 #include <stdlib.h>
+#include <libc.h>
 #include "fractol.h"
 #include "lib/libft/libft.h"
 
 int	main(int argc, char **argv)
 {
-	if (argc == 2 && (ft_memcmp("mandelbrot", argv[1], 11)
-			|| ft_memcmp("m", argv[1], 2)))
+	if (argc == 2 && (!ft_memcmp("mandelbrot", argv[1], 11)
+			|| !ft_memcmp("m", argv[1], 2)))
 		fr_mandelbrot();
-	else if (argc == 2 && (ft_memcmp("julia", argv[1], 6)
-			|| ft_memcmp("j", argv[1], 2)))
+	else if (argc == 2 && (!ft_memcmp("julia", argv[1], 6)
+			|| !ft_memcmp("j", argv[1], 2)))
 		fr_julia();
 	else
 	{
-		ft_printf("Illigal Option");
-		ft_printf("Usage:");
-		ft_printf("\t%s [mandelbrot | julia | m | j]", argv[0]);
+		ft_printf("Illigal Option\n");
+		ft_printf("Usage:\n");
+		ft_printf("\t%s [mandelbrot | julia | m | j]\n", argv[0]);
 		return (1);
 	}
 	return (0);
